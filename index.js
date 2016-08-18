eab4ccd66c93198f582daa904baccdb9ec55367b
olahfemi <olahfemi@gmail.com>
2016-08-18 16:58:13 +0100

    It works! Eureka!

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function NotesApplication(author) {
 	this.author = author;
 	this.notes = [];
 	
-	
 	this.create = function(note_content){
 		this.note_content = note_content;
 		this.notes.push(note_content);
-		return this.notes;
+		return "Success";
+		
 	};
 }
 module.exports = NotesApplication;
--- test/index-spec.js
+++ test/index-spec.js
@@ -1,6 +1,5 @@
-var chai = require ("chai");
+var expect = require ("chai").expect;
 var NotesApplication = require("../index.js");
-var expect = chai.expect;
 var work = new NotesApplication("Femi")
 
 describe ("This class called NotesApplication", function(){
@@ -9,7 +8,7 @@ describe ("This class called NotesApplication", function(){
 	describe("has a function called 'create'", function(){
 		it("that a return a note in an array", function(){
 			//var made = work.create("Andela");
-			expect(work.create('Andela')).to.equal(['Andela']);
+			expect(work.create("Andela")).to.equal("Success");
 		});
 		
 	});
