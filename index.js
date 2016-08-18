47fbb7e5675bfa7abb15b35a84313fb0277d356a
olahfemi <olahfemi@gmail.com>
2016-08-18 12:32:34 +0100

    Test began.

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports = NotesApplication;
+function NotesApplication() {
+	
+}
---
+++ test/index-spec.js
@@ -0,0 +1,7 @@
+var chai = require ("chai");
+var index = require("../index");
+var expect = chai.expect;
+
+describe ("A class called NotesApplication", function(){
+	
+});
